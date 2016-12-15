0b2c7208225b3ab7fb95adcd3e2eb55fe8231305
Vasil Dakov <vasildakov@gmail.com>
2016-12-15 16:51:42 +0000

    Updated model

--- src/Model/Duck/MallardDuck.php
+++ src/Model/Duck/MallardDuck.php
@@ -1,10 +1,25 @@
 <?php
+/**
+ * This file is part of the vasildakov/duck project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Vasil Dakov <vasildakov@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @link https://github.com/vasildakov/duck GitHub
+ */
 
 namespace CoI\Model\Duck;
 
-class MallardDuck extends Duck
+/**
+ * Class MallardDuck
+ *
+ * @package Model
+ * @author Vasil Dakov <vasildakov@gmail.com>
+ */
+final class MallardDuck extends Duck
 {
-
     public function display()
     {
         return "I'm a real Mallard duck";
--- src/Model/Duck/Quack/Quack.php
+++ src/Model/Duck/Quack/Quack.php
@@ -1,7 +1,23 @@
 <?php
+/**
+ * This file is part of the vasildakov/duck project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Vasil Dakov <vasildakov@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @link https://github.com/vasildakov/duck GitHub
+ */
 
 namespace CoI\Model\Duck\Quack;
 
+/**
+ * Class Quack
+ *
+ * @package Model
+ * @author Vasil Dakov <vasildakov@gmail.com>
+ */
 class Quack implements Quackable
 {
     public function quack()
